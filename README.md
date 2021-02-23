9109ff9b4415c5b211f65752120099e7bb7aa798
rupali <rups.mahajan1@gmail.com>
2021-02-23 17:56:17 +1100

    Added all other script command line directives related code
    Signed-off-by: rupali <rups.mahajan1@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # php-task
+
+1) Script Task
--- user_upload.php
+++ user_upload.php
@@ -15,12 +15,12 @@ if ($dbConnection->connect_error) {
 echo "Connected successfully \n";
 
 
-getInputCommand($dbConnection);
+getInputCommand($dbConnection, $serverName, $userName, $password);
 /* * ***************************
  * Function to handle commands from command line
  * *********************** */
 
-function getInputCommand($dbConnection) {
+function getInputCommand($dbConnection, $serverName = "localhost", $userName = "root", $password = "") {
     //Get input 
     $getCommand = readline("Enter the command (use --help to see all command line options) : ");
 
@@ -31,13 +31,17 @@ function getInputCommand($dbConnection) {
     if (strcmp(trim($getCommand), "--help") == 0) {
         getHelp();
     } elseif (strcmp(trim($splitCommand[0]), "--file") == 0) {
+        $dryRun = false;
         //check if file name is provided or not
         if (isset($splitCommand[1])) {
             $fileName = trim($splitCommand[1]);
             //check input file is exists or not
             if ((strcmp($fileName, "users.csv") == 0) && file_exists(realpath(trim($fileName))) == 1) {
+                if (trim($splitCommand[2]) == '--dry_run') {
+                    $dryRun = true;
+                }
                 //if file exists save  data to database
-                executeFile($fileName, $dbConnection);
+                executeFile($fileName, $dbConnection, $dryRun, $serverName, $userName, $password);
             } else
             if (strcmp($fileName, "users.csv") && file_exists(realpath("users.csv") == false)) {
                 echo "users.csv does not exist.\n";
@@ -50,6 +54,20 @@ function getInputCommand($dbConnection) {
             echo "try --help for more information";
             exit();
         }
+    } elseif (strcmp(trim($splitCommand[0]), "--create_table") == 0) {
+        createTable();
+    } elseif (strcmp(trim($splitCommand[0]), "-u") == 0) {
+        echo "MySQL Username " . $userName . "\n";
+        getInputCommand($dbConnection, $serverName, $userName, $password);
+    } elseif (strcmp(trim($splitCommand[0]), "-p") == 0) {
+        echo "MySQL Password " . $password . "\n";
+        getInputCommand($dbConnection, $serverName, $userName, $password);
+    } elseif (strcmp(trim($splitCommand[0]), "-h") == 0) {
+        echo "MySQL Host " . $serverName . "\n";
+        getInputCommand($dbConnection, $serverName, $userName, $password);
+    } else {
+        echo "Invalid command.Please enter a valid command.\n";
+        getInputCommand($dbConnection, $serverName, $userName, $password);
     }
 }
 
@@ -58,13 +76,13 @@ function getInputCommand($dbConnection) {
  * store all csv file data to database
  * @param - file Name
  *          database connection
+ *          Dry run - execute file with create table or not
  */
 
-function executeFile($fileName = 'users.csv', $dbConnection) {
+function executeFile($fileName = 'users.csv', $dbConnection, $dryRun = false, $serverName, $userName, $password) {
 
     //create table
     createTable($dbConnection);
-
     //open csv file
     $file = fopen($fileName, "r");
     $i = 1;
@@ -72,17 +90,21 @@ function executeFile($fileName = 'users.csv', $dbConnection) {
         $fileArray = fgetcsv($file);
         $name = ucfirst($fileArray[0]);
         $surname = ucfirst($fileArray[1]);
-        //validate email address
-        if (validateEmailAddress($fileArray[2]) === True) {
-            $email = trim(strtolower($fileArray[2]));
-            $insertQuery = 'INSERT INTO userDetails (name, surname, email)
+        if ($dryRun == false) {
+            //validate email address
+            if (validateEmailAddress($fileArray[2]) === True) {
+                $email = trim(strtolower($fileArray[2]));
+                $insertQuery = 'INSERT INTO userDetails (name, surname, email)
             VALUES ("' . $name . '", "' . $surname . '", "' . $email . '")';
-            if ($dbConnection->query($insertQuery) === TRUE) {
-                $count = $i++;
-            } else {
-                //echo "\n Error: " . $insertQuery . "<br>" . $dbConnection->error;
-                echo "\n Error: " . $dbConnection->error . "\n";
+                if ($dbConnection->query($insertQuery) === TRUE) {
+                    $count = $i++;
+                } else {
+                    //echo "\n Error: " . $insertQuery . "<br>" . $dbConnection->error;
+                    echo "\n Error: " . $dbConnection->error . "\n";
+                }
             }
+        } else {
+            getInputCommand($dbConnection, $serverName, $userName, $password);
         }
     }
     echo "\n" . $i . "Users inserted successfully";
@@ -90,6 +112,7 @@ function executeFile($fileName = 'users.csv', $dbConnection) {
 
 /* * ****************************
  * Function to create tabel using database connections
+ * @param - database connection
  */
 
 function createTable($dbConnection) {
